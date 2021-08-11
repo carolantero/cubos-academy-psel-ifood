269a220eebb692a44aabfa899f88bfac6f469c7f
Carolina Antero <carolina.antero.santos@gmail.com>
2021-08-11 19:45:17 -0300

    commitando os escondidos

---
+++ esconde_esconde.js
@@ -0,0 +1,10 @@
+function solucao(numero) {
+	//seu codigo aqui
+    if (numero > 20) {
+        for(let indice = 21; numero >= indice; indice++) {
+            console.log (indice);
+        }
+    } else {
+        console.log ("VOCE CONSEGUE, MANINHO");
+    }
+}
